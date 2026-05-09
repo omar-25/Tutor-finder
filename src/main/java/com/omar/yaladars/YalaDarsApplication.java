2a7720c0f5758ed6bc52679d942843d3eaa76693
omar-25 <omarahmed252612@gmail.com>
2026-05-09 16:13:44 +0300

    first commit

---
+++ src/main/java/com/omar/yaladars/YalaDarsApplication.java
@@ -0,0 +1,13 @@
+package com.omar.yaladars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YalaDarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YalaDarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omar/yaladars/YalaDarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.yaladars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YalaDarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
