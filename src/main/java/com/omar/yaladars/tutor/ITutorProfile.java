4390febbafd45b356eaec4dc6ca89549d543e51b
KareemCodez <kareem.amr.1973@gmail.com>
2026-05-23 17:06:04 +0300

    tutor

---
+++ src/main/java/com/omar/yaladars/tutor/ITutorProfile.java
@@ -0,0 +1,12 @@
+package com.omar.yaladars.tutor;
+
+import java.util.UUID;
+
+public interface ITutorProfile {
+    Tutor createTutorProfile(UUID userId, TutorProfileDTO dto);
+    Tutor getTutorProfile(UUID tutorId);
+    Tutor updateTutorProfile(UUID tutorId, TutorProfileDTO dto);
+    void deleteTutorProfile(UUID tutorId);
+    Double getTutorRating(UUID tutorId);
+
+}
