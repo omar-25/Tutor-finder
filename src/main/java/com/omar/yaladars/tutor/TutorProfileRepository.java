36884ad958c09ade8321915ae4a63b9b8a4c7bff
omar-25 <omarahmed252612@gmail.com>
2026-05-23 17:25:34 +0300

    Delete src/main/java/com/omar/yaladars/tutor directory

--- src/main/java/com/omar/yaladars/tutor/TutorProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.omar.yaladars.tutor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.UUID;
-
-@Repository
-public interface TutorProfileRepository extends JpaRepository<Tutor, UUID> {
-}
